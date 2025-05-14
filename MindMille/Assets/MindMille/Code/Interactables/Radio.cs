da7324ab90e61c2ce25aae22b36a078ba954d99e
BlakesDV <dmunoz@cocoschool.mx>
2025-05-14 11:21:25 -0600

    TODOS

---
+++ MindMille/Assets/MindMille/Code/Interactables/Radio.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Radio : MonoBehaviour
+{
+    //TODO: Funcion para que se encienda al llegar a cierta zona
+    //TODO: Funcion para que pueda "Buscar el canal correcto" ej: estatica en cualquier canal que no sea el 7
+    //TODO: Funcion para ajustar el volumen del radio
+}
