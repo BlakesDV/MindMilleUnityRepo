cfb45d4276702a1ebde86c5f5af901c5cfad0c0b
BlakesDV <dmunoz@cocoschool.mx>
2025-05-21 01:58:12 -0600

    PauseMenu Implementation

--- MindMille/Assets/MindMille/Code/Menus/SceneMngr.cs
+++ MindMille/Assets/MindMille/Code/Menus/SceneMngr.cs
@@ -3,13 +3,32 @@ using UnityEngine.SceneManagement;
 
 public class SceneMngr : MonoBehaviour
 {
+    public GameObject pauseMenu;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseTheGame();
+        }
+    }
     public void ChangeScene(int index)
     {
         SceneManager.LoadScene(index);
     }
 
-    public void QuitGame()
+    public void QuitTheGame()
     {
         Application.Quit();
+        Debug.Log("Quited");
+    }
+
+    public void PauseTheGame()
+    {
+        pauseMenu.SetActive(true);
+    }
+    public void UnpauseTheGame()
+    {
+        pauseMenu.SetActive(false);
     }
 }
