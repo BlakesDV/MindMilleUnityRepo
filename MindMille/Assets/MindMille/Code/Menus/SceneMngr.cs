cfc5e339f06a1f365963bce99509a5d369ec96c4
BlakesDV <dmunoz@cocoschool.mx>
2025-05-20 21:56:14 -0600

    Scene Manager Flow

---
+++ MindMille/Assets/MindMille/Code/Menus/SceneMngr.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMngr : MonoBehaviour
+{
+    public void ChangeScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
